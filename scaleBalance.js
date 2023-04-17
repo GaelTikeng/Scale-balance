58aec145dcb7f1cf108e62a37f1bddd7a08f78f1
GaelTikeng <Gaelinho@yahoo.fr>
2023-04-17 22:47:21 +0100

    sorting the array in ascending order

--- scaleBalance.js
+++ scaleBalance.js
@@ -5,14 +5,13 @@ const calculate = document.querySelector('.balance')
 
 calculate.addEventListener('click', () => {
   const newArr = JSON.parse(arr.value)
-  newArr.sort(function(a, b) {return a - b})
+  newArr.sort(function (a, b) { return a - b })
   for (let i = 0; i <= newArr.length; i++) {
     for (let j = 0; j <= newArr.length; j++) {
       if ((+l.value + newArr[i]) === (+r.value + newArr[j])) {
         document.querySelector('.displaynum').innerHTML = `${newArr[i]} and ${newArr[j]} are needed to balance ${l.value} and ${r.value} respectively`
         return
-      }
-      else if (i === newArr.length - 1) {
+      } else if (i === newArr.length - 1) {
         document.querySelector('.display').innerHTML = 'Cannot Balance'
       }
     }
