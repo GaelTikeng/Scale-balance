233b6f3c7e4437a7d5db7cba8bcc1c2c121b7366
GaelTikeng <Gaelinho@yahoo.fr>
2023-11-02 17:14:02 +0100

    linters

--- assets/script/scaleBalance.js
+++ assets/script/scaleBalance.js
@@ -1,25 +1,25 @@
-const leftWeight = document.getElementById("leftside");
-const rightWeight = document.getElementById("rightside");
-const arr = document.getElementById("scale");
-const calculate = document.querySelector(".balance");
+const leftWeight = document.getElementById("leftside")
+const rightWeight = document.getElementById('rightside')
+const arr = document.getElementById('scale')
+const calculate = document.querySelector('.balance')
 
-calculate.addEventListener("click", () => {
-  if ((leftWeight.value === "") | (rightWeight.value === "") | (arr === "")) {
-    console.log("Please fill all fields");
-    document.querySelector(".displaynum").innerHTML = "Please fill all fields";
+calculate.addEventListener('click', () => {
+  if ((leftWeight.value === '') | (rightWeight.value === '') | (arr === '')) {
+    console.log('Please fill all fields')
+    document.querySelector(".displaynum").innerHTML = 'Please fill all fields'
   } else {
     let myArr = String(arr.value)
-      .split(",")
+      .split(',')
       .map((num) => {
-        return Number(num);
-      });
-    console.log(typeof myArr);
-    console.log(myArr);
+        return Number(num)
+      })
+    console.log(typeof myArr)
+    console.log(myArr)
     let sortedArr = myArr.sort(function (a, b) {
-      return a - b;
-    });
-    console.log(typeof sortedArr);
-    console.log(sortedArr);
+      return a - b
+    })
+    console.log(typeof sortedArr)
+    console.log(sortedArr)
     for (let i = 0; i <= sortedArr.length; i++) {
       for (let j = 0; j <= sortedArr.length; j++) {
         if (
@@ -27,11 +27,11 @@ calculate.addEventListener("click", () => {
           +rightWeight.value + sortedArr[j]
         ) {
           document.querySelector(
-            ".displaynum"
-          ).innerHTML = `${sortedArr[i]} and ${sortedArr[j]} are needed to balance ${leftWeight.value} and ${rightWeight.value} respectively`;
+            '.displaynum'
+          ).innerHTML = `${sortedArr[i]} and ${sortedArr[j]} are needed to balance ${leftWeight.value} and ${rightWeight.value} respectively`
           return;
         } else if (i === sortedArr.length - 1) {
-          document.querySelector(".displaynum").innerHTML = "Cannot Balance";
+          document.querySelector('.displaynum').innerHTML = 'Cannot Balance'
         }
       }
     }
