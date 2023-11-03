4a6242b80c61aa09f60b7709f30e9a2d990f769f
GaelTikeng <Gaelinho@yahoo.fr>
2023-11-03 04:23:17 +0100

    adding requested changesa

--- assets/script/scaleBalance.js
+++ assets/script/scaleBalance.js
@@ -1,4 +1,4 @@
-const leftWeight = document.getElementById("leftside")
+const leftWeight = document.getElementById('leftside')
 const rightWeight = document.getElementById('rightside')
 const arr = document.getElementById('scale')
 const calculate = document.querySelector('.balance')
@@ -6,16 +6,16 @@ const calculate = document.querySelector('.balance')
 calculate.addEventListener('click', () => {
   if ((leftWeight.value === '') | (rightWeight.value === '') | (arr === '')) {
     console.log('Please fill all fields')
-    document.querySelector(".displaynum").innerHTML = 'Please fill all fields'
+    document.querySelector('.displaynum').innerHTML = 'Please fill all fields'
   } else {
-    let myArr = String(arr.value)
+    const myArr = String(arr.value)
       .split(',')
       .map((num) => {
         return Number(num)
       })
     console.log(typeof myArr)
     console.log(myArr)
-    let sortedArr = myArr.sort(function (a, b) {
+    const sortedArr = myArr.sort(function (a, b) {
       return a - b
     })
     console.log(typeof sortedArr)
@@ -29,11 +29,11 @@ calculate.addEventListener('click', () => {
           document.querySelector(
             '.displaynum'
           ).innerHTML = `${sortedArr[i]} and ${sortedArr[j]} are needed to balance ${leftWeight.value} and ${rightWeight.value} respectively`
-          return;
+          return
         } else if (i === sortedArr.length - 1) {
           document.querySelector('.displaynum').innerHTML = 'Cannot Balance'
         }
       }
     }
   }
-});
+})
